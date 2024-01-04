8cccc1723cd96978fb12613220f88092e1089010
M. Gholam <mgholam@gmail.com>
2024-01-04 08:49:24 +0330

    - network info

--- pidash.js
+++ pidash.js
@@ -11,23 +11,30 @@ function sysinfo(){
     var cpuinfo = execSync('/bin/sh -c "cat /proc/cpuinfo"').toString();
     var disks = execSync('/bin/df -h').toString();
     var memory = execSync('/usr/bin/free -h').toString();
-
+    
     // 19:21:06 up 14 min,  0 users,  load average: 0.23, 0.38, 0.45
     // 11:15:25 up 2 days, 16 min,  0 users,  load average: 0.16, 0.03, 0.01
-    var str = execSync('/usr/bin/uptime').toString();
+    var str = execSync('/usr/bin/uptime').toString(); 
     var ss = str.split("  ");
     var load = ss[ss.length-1];
     var users = ss[ss.length-2];
     var uptime = str.replace(load,"").replace(users,"");
+    var network="'vnstat' command not found, please install";
+    try{
+        var nstr = execSync('/usr/bin/vnstat --oneline').toString();
+        ss = nstr.split(";");
+        network = ss[1] + " (" + ss[2] +  ") : recieve = " + ss[3] + ", send = " + ss[4];
+    }catch{}
 
     var o = {
-        uptime ,
-        users ,
-        load ,
+        uptime,
+        users,
+        load,
         temp,
         cpuinfo,
-        disks,
-        memory
+        disks, 
+        memory,
+        network
     };
     return JSON.stringify(o);
 }
