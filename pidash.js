74ab165696bd6d4373416cf776e13c2ae67412b6
M. Gholam <mgholam@gmail.com>
2024-01-29 18:41:34 +0330

    - uname info - network ip info

--- pidash.js
+++ pidash.js
@@ -1,9 +1,25 @@
 var http = require('http');
+var os = require('os');
 var fs = require('fs');
 
 const { execSync } = require('child_process');
-var index = fs.readFileSync('index.html').toString();
+// var index = fs.readFileSync('index.html').toString();
 var skeleton = fs.readFileSync('skeleton.css');
+
+
+function getip() {
+    var interf = os.networkInterfaces();
+    var addr = [];
+    for (var k in interf){
+        for (var k2 in interf[k]){
+            var ad = interf[k][k2];
+            if (ad.family === 'IPv4' && !ad.internal) {
+                addr.push(ad.address);
+            }
+        }
+    }
+    return addr;
+}
 
 function sysinfo(){
     var temp = execSync('/bin/sh -c "cat /sys/class/thermal/thermal_zone0/temp"').toString();
@@ -11,19 +27,23 @@ function sysinfo(){
     var cpuinfo = execSync('/bin/sh -c "cat /proc/cpuinfo"').toString();
     var disks = execSync('/bin/df -h').toString();
     var memory = execSync('/usr/bin/free -h').toString();
-    
-    // 19:21:06 up 14 min,  0 users,  load average: 0.23, 0.38, 0.45
-    // 11:15:25 up 2 days, 16 min,  0 users,  load average: 0.16, 0.03, 0.01
-    var str = execSync('/usr/bin/uptime').toString(); 
+    var uname = execSync('/bin/uname -a').toString();
+    // return format for 'uptime':
+    //     19:21:06 up 14 min,  0 users,  load average: 0.23, 0.38, 0.45
+    //     11:15:25 up 2 days, 16 min,  0 users,  load average: 0.16, 0.03, 0.01
+    var str = execSync('/usr/bin/uptime').toString();
     var ss = str.split("  ");
     var load = ss[ss.length-1];
     var users = ss[ss.length-2];
     var uptime = str.replace(load,"").replace(users,"");
+    var ipaddr = "";
+    getip().forEach(i=> ipaddr += i + "\n");
+
     var network="'vnstat' command not found, please install";
     try{
         var nstr = execSync('/usr/bin/vnstat --oneline').toString();
         ss = nstr.split(";");
-        network = ss[1] + " (" + ss[2] +  ") : receive = " + ss[3] + ", send = " + ss[4];
+        network = ss[1] + " (" + ss[2] +  ") :\nrecieve = " + ss[3] + ", send = " + ss[4];
     }catch{}
 
     var o = {
@@ -32,9 +52,11 @@ function sysinfo(){
         load,
         temp,
         cpuinfo,
-        disks, 
+        disks,
         memory,
-        network
+        network,
+	    ipaddr,
+	    uname,
     };
     return JSON.stringify(o);
 }
@@ -43,6 +65,7 @@ var server = http.createServer(function (req, res) {
     try {
         if(req.url == '/'){
             res.writeHead(200, { 'Content-Type': 'text/html' });
+            var index = fs.readFileSync('index.html').toString();
             var o = index.replace("//d=", "d=" + sysinfo());
             res.write(o);
             res.end();
